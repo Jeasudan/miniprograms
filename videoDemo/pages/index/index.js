449bac9a21bbb816288dd6887c3a7f5ffa848086
Jeasudan <1778919601@qq.com>
2020-02-27 13:35:04 +0800

    微信小程序视频的使用

---
+++ videoDemo/pages/index/index.js
@@ -0,0 +1,116 @@
+// pages/index/index.js
+function getRandomColor(){
+  let rgb = []
+  for(let i = 0;i<3;i++){
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = (color.length === 1) ? '0'+  color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    src:'',
+    list: [
+      {
+        id:'1',
+        title: '第一个视频',
+        videoUrl:'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+      },
+      {
+        id: '2',
+        title: '第二个视频',
+        videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+      },
+      {
+        id: '3',
+        title: '第三个视频',
+        videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+      },
+      {
+        id: '4',
+        title: '第四个视频',
+        videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400'
+      },
+    ],
+    danmuTxt:''
+  },
+  getDanmu:function(e){
+    this.setData({
+      danmuTxt:e.detail.value
+    })
+  },
+  sendDanmu(e){
+    let text = this.data.danmuTxt
+    this.videoCtx.sendDanmu({
+      text:text,
+      color: getRandomColor()
+    })
+  },
+  playVideo:function(e){
+    this.videoCtx.stop()
+    this.setData({
+      src:e.currentTarget.dataset.url
+    })
+    this.videoCtx.play()
+  },
+   /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.videoCtx = wx.createVideoContext('myVideo')
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
