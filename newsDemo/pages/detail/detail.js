f2e9df21cbf95d663d8f0b4f52d65608c073d961
Jeasudan <1778919601@qq.com>
2020-03-03 22:43:56 +0800

    三个页面结束

---
+++ newsDemo/pages/detail/detail.js
@@ -0,0 +1,72 @@
+// pages/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    article: {
+      id: '356412',
+      title: '特写：2019，天安门城楼前的中国',
+      poster: 'http://image1.chinanews.com.cn/cnsupload/big/2019/10-01/4-426/a7e426b0dd6c43d2bc710fafe810a0d5.jpg',
+      add_date: '2019-10 - 01',
+      content: '   对于普通民众而言,一场阅兵式,军事装备展示无疑最令人期待。这个昔日饱经炮火、战乱洗礼的民族,此刻正在集中向世界展示70年来国家的发展进步。\n    习近平曾说,"我经常看中国近代的一些史料, 一看到落后挨打的悲惨场景就痛彻肺腑！” 谈及百余年前的甲午战争， 他称之为“ 剜心之痛”。'
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
---
+++ newsDemo/pages/my/my.js
@@ -0,0 +1,75 @@
+// pages/my/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nickName:'未登录',
+    src:'/images/index1.png',
+    newsList:[
+      {
+        id: '356412',
+        title: '特写：2019，天安门城楼前的中国',
+        poster: 'http://image1.chinanews.com.cn/cnsupload/big/2019/10-01/4-426/a7e426b0dd6c43d2bc710fafe810a0d5.jpg',
+        add_date: '2019 - 10 - 01'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
